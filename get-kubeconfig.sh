85ec35f861566c931b15dfe2cc5dfdb75a9ac90f
Remco Beckers <remco.beckers@gmail.com>
2021-08-17 11:01:51 +0200

    Fix get-kubeconfig.sh for k3s new version

--- get-kubeconfig.sh
+++ get-kubeconfig.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-KUBE_CONFIG=/kubeconfig
-if [ ! -z "${K3S_KUBECONFIG_OUTPUT_DIR}" ]; then
-    KUBE_CONFIG=${K3S_KUBECONFIG_OUTPUT_DIR}/kubeconfig
-fi
-
-export KUBECONFIG=${KUBE_CONFIG}
-
-while [ ! -f ${KUBE_CONFIG} ]; do
-    sleep 1
-done
-
 if [ "$1" = "-json" ]; then
     cfg=$(kubectl config view -o json --merge=true --flatten=true)
 else
